ac06c3350f18251ee20c01053f3f28bc252179d0
Admin <jojo484805@gmail.com>
2026-01-07 11:30:25 +0700

    Close #4

--- Entity/Books.go
+++ Entity/Books.go
@@ -7,6 +7,7 @@ import (
 type Books struct {
 	gorm.Model
 
+	
 	Title string  `valid="length(3|100)~Title must be between 3 and 100"`
 	Price float64 `valid="range(50|5000)~Price must be between 50 and 5000"`
 	Code  string  `valid="matches=(^BK//d6)~Code must start with BK followed by 6 digit(0-9)"`
